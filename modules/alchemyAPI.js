9cb5b2028a39a815aebb2d77b47edd802f957c1a
dlacroixe <lukatoni9@gmail.com>
2013-09-04 11:47:52 +0200

    achemy chnages

--- modules/alchemyAPI.js
+++ modules/alchemyAPI.js
@@ -1,39 +1,27 @@
 
 var AlchemyAPI = require('alchemy-api');
 
-
-
 function alchemyAPI () {
 
 	var alchemy = new AlchemyAPI('74631f889f9d816cc4aae9e38dbe8d5cd4b9787b');
 
-	this.keywords = function () {
+	this.keywords = function (testURL, callback) {
 		alchemy.keywords(testURL, {}, function(err, response) {
 		  if (err) throw err;
 
-		  // See http://www.alchemyapi.com/api/ for format of returned object
-
 		  var keywords = response.keywords;
-		  console.log("keywords");
-		  console.log(keywords);
-		  //console.log(response);
+		  callback(keywords);
 
-		  // Do something with data
 		});
 	}
 
-	this.entities = function () {
+	this.entities = function (testURL, callback) {
 		alchemy.entities(testURL, {}, function(err, response) {
 		  if (err) throw err;
 
-		  // See http://www.alchemyapi.com/api/ for format of returned object
-
 		  var entities = response.entities;
-		  console.log("entidades");
-		  console.log(entities);
-		  //console.log(response);
+		  callback(entities);
 
-		  // Do something with data
 		});
 	}
 
@@ -41,15 +29,9 @@ function alchemyAPI () {
 		alchemy.category(testURL, {}, function(err, response) {
 		  if (err) throw err;
 
-		  // See http://www.alchemyapi.com/api/ for format of returned object
-
 		  var category = response.category;
-		  console.log("category");
-		  console.log(category);
 		  callback(category)
-		  //console.log(response);
 
-		  // Do something with data
 		});
 	}
 }
